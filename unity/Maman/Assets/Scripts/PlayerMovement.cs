a2594070cd11ef70165006f2a50ebd26fd5d8b42
Matias Lecaros <m.lecaros.aranis@gmail.com>
2023-07-27 23:06:40 -0400

    CharacterController2D

--- unity/Maman/Assets/Scripts/PlayerMovement.cs
+++ unity/Maman/Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController rb2D;
+    public CharacterController2D controller;
+
+    [SerializeField] private float moveSpeed = 40f;
+    [SerializeField] private float gravity = 9.81f;
+
     private Vector2 movementInput;
 
-    [SerializeField] private float moveSpeed = 5f;
-
+    private float moveDirection;
+    private bool jump = false;
     private void Awake()
     {
-        // Get the Rigidbody2D component attached to the "Player" game object (if any).
-        rb2D = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController2D>();
     }
 
     private void Update()
     {
-        // Check if the Rigidbody2D component is attached before using it.
-        //if (rb2D.Equals(null))
-        if (rb2D != null)
-        {
-            //Debug.LogWarning("Character controller component is OK.");
+        moveDirection = movementInput.x * moveSpeed;
+    }
 
-        }
-        else
-        {
-            // If the Rigidbody2D component is not attached, you can handle it gracefully here
-            // by printing a warning or adding the component programmatically, depending on your use case.
-            Debug.LogWarning("Character controller component is missing from the Player game object.");
-        }
+    private void FixedUpdate()
+    {
+        controller.Move(moveDirection*Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
     
     // This method is called from the PlayerInput component using the "sent messages" approach.
@@ -36,12 +34,19 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.LogWarning("On Move triggered...");
 
-        // Get the movement input vector from the PlayerInput component.
-        // The control type is set to "Vector2" in the PlayerInput component, and action type is "Value".
+        // Get the movement input vector from the event.
+        // The control type is set to "Vector2" in the PlayerInput component.
         movementInput = value.Get<Vector2>();
-        rb2D.SimpleMove(movementInput * moveSpeed);
         
         Debug.LogWarning("On Move ended...");
 
+    }
+
+    public void OnJump()
+    {
+        Debug.LogWarning("On Jump triggered...");
+        jump = true;
+
+        Debug.LogWarning("On Jump ended...");
     }
 }
